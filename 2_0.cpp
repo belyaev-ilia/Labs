794eeb95b8367d7cae1af77c87975b85146c0450
belyaev-ilia <ilia.belyaev2005@gmail.com>
2025-04-10 00:10:26 +0300

    Add files via upload

---
+++ 2_0.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <string>
+
+class Character {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    // Конструктор
+    Character(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Character " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Character() {
+        std::cout << "Character " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+            << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};
+
+class Monster {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    // Конструктор
+    Monster(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Monster " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Monster() {
+        std::cout << "Monster " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+            << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};
+
+class Weapon {
+private:
+    std::string name;
+    int damage;
+    int weight;
+public:
+    // Конструктор
+    Weapon(const std::string& n, int d, int w)
+        : name(n), damage(d), weight(w) {
+        std::cout << "Weapon " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Weapon() {
+        std::cout << "Weapon " << name << " destroyed!\n";
+    }
+    
+    //Переопределение displayInfo
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", Damage: " << damage << ", Weight: " << weight << std::endl;
+    }
+};
+
+int main()
+{
+    Weapon sword("Sword", 40, 15);
+    Weapon dagger("Dagger", 10, 5);
+    sword.displayInfo();
+    dagger.displayInfo();
+    return 0;
+}
